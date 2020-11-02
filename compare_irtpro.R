e278f5114d124bb205efc8943434bdf87efd41f7
Isaac Pfleger <36799604+Pflegermeister@users.noreply.github.com>
2020-11-02 11:09:15 -0800

    Update compare_irtpro.R

--- compare_irtpro.R
+++ compare_irtpro.R
@@ -2,5 +2,14 @@ library(wizirt)
 
 data('responses')
 data <- responses[,-1]
-print(my_model, type = 'item')
-print(my_model, type = 'person')
+my_model <- wizirt(data = data, item_type = 'Rasch')
+print(my_model, type = 'item') %>% print(n = 25)
+
+my_model <- wizirt(data = data, item_type = '1PL')
+print(my_model, type = 'item') %>% print(n = 25)
+
+my_model <- wizirt(data = data, item_type = '2PL')
+print(my_model, type = 'item') %>% print(n = 25)
+
+my_model <- wizirt(data = data, item_type = '3PL')
+print(my_model, type = 'item') %>% print(n = 25)
